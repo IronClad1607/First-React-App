b79b1c9085541488cba93ee2b89eaf691430abcd
Ishaan Jain <ishaan160799@gmail.com>
2020-10-11 23:54:52 +0530

    components screen ready

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,2 +1,14 @@
 import React from 'react'
-import {Text,Stylesheet} from 'react-native'
+import { Text, StyleSheet } from 'react-native'
+
+const ComponentsScreen = () => {
+    return <Text style={styles.textStyle}> This is components Screen </Text>
+}
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize = 30
+    }
+})
+
+export default ComponentsScreen
