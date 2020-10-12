0c0fe5879a0ce071827b78ad8327b31c6451eb5f
Ishaan Jain <ishaan160799@gmail.com>
2020-10-12 18:03:44 +0530

    exercise done

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,20 +2,23 @@ import React from 'react'
 import { Text, StyleSheet, View } from 'react-native'
 
 const ComponentsScreen = () => {
-    const greeting = 'Bye there!'
-    const jsxVariable = <Text>I am IronClad</Text>
+    const name = 'Ishaan'
 
 
-    return (<View>
-        <Text style={styles.textStyle}>{greeting}</Text>
-        <Text>Hi there</Text>
-        {jsxVariable}
-    </View>)
-}
+    return (
+        <View>
+            <Text style={styles.headingStyle}>Getting started with React Native</Text>
+            <Text style={styles.subHeaderStyle}>My name is {name} </Text>
+        </View>
+    )
+}   
 
 const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 30
+    headingStyle: {
+        fontSize: 45
+    },
+    subHeaderStyle: {
+        fontSize: 20
     }
 })
 
