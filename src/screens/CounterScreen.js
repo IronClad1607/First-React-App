f676ff77954ff33feeeaf384033299f8bb5e5f49
Ishaan Jain <ishaan160799@gmail.com>
2020-10-12 23:34:24 +0530

    first state example started

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 
 const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View>
-      <Text>Current Count:</Text>
+      <Button
+        title="Increase"
+        styles={styles.button}
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      />
+      <View style={styles.space} />
+      <Button
+        title="Decrease"
+        style={styles.button}
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
+      <View style={styles.space} />
+      <Text>Current Count:{counter}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  button: {
+    marginBottom: 20,
+    padding: 30,
+  },
+  space: {
+    width: 20,
+    height: 20,
+  },
+});
 
-export default CounterScreen
+export default CounterScreen;
