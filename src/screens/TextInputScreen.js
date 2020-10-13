572f50c35e3e19efd7f9b13e9f0d26314750c630
Ishaan Jain <ishaan160799@gmail.com>
2020-10-13 21:37:20 +0530

    text input added

--- src/screens/TextInputScreen.js
+++ src/screens/TextInputScreen.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import {Text,View,StyleSheet,Button} from 'react-native'
+import React from "react";
+import { Text, View, StyleSheet, TextInput } from "react-native";
 
 const TextInputScreen = () => {
-    return (<View>
-        <Text>This is Text Input</Text>
-    </View>)
-}
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
 
-
-export default TextInputScreen
+export default TextInputScreen;
